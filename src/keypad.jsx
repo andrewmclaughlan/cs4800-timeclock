b882a6e828930337f509122e5cf9b6020469c397
andrewmclaughlan <112527920+andrewmclaughlan@users.noreply.github.com>
2024-02-01 09:11:34 -0700

    updated keypad.jsx

--- src/keypad.jsx
+++ src/keypad.jsx
@@ -12,7 +12,7 @@ import {
 } from '@chakra-ui/react'
 //This is a functional keypad with test data
 function Keypad () {
-  const pins = ['111111', '12211', '88888888', '1111', '111', '1234567890'];
+  const pins = ['123', '1234', '88888888', '1111', '1234567890'];
   const [pin, setPin] = useState('');
   const[punchMessage, setPunchMessage] = useState();
    return (
@@ -75,53 +75,6 @@ function Keypad () {
    );
    
 }
-//This is what OpenChakra will make for you
-/*
-const Keypad = () => ( 
-  <Container>
-    <InputGroup display="inline-block">
-      <Input type='password' variant="outline" size="lg" />
-      <Grid templateColumns="repeat(3, 1fr)" gap={6} column={3} row={4}>
-        <Button variant="solid" size="md">
-          1
-        </Button>
-        <Button variant="solid" size="md">
-          2
-        </Button>
-        <Button variant="solid" size="md">
-          3
-        </Button>
-        <Button variant="solid" size="md">
-          4
-        </Button>
-        <Button variant="solid" size="md">
-          5
-        </Button>
-        <Button variant="solid" size="md">
-          6
-        </Button>
-        <Button variant="solid" size="md">
-          7
-        </Button>
-        <Button variant="solid" size="md">
-          8
-        </Button>
-        <Button variant="solid" size="md">
-          9
-        </Button>
-        <Button variant="solid" size="md" colorScheme="red">
-          C
-        </Button>
-        <Button variant="solid" size="md">
-          0
-        </Button>
-        <Button variant="solid" size="md" colorScheme="whatsapp">
-          Enter
-        </Button>
-      </Grid>
-    </InputGroup>
-  </Container>
-)*/
 
 const App = () => (
   <ChakraProvider resetCSS>
