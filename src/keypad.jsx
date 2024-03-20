f5d8b4306b258db906e2dc33544693cb2c133429
andrewmclaughlan <112527920+andrewmclaughlan@users.noreply.github.com>
2024-03-20 16:18:28 -0600

    added console command electron++ to package.json. keypad.jsx now authenticates using the database.

--- src/keypad.jsx
+++ src/keypad.jsx
@@ -10,9 +10,13 @@ import {
   InputGroup,
   Text
 } from '@chakra-ui/react'
+import { emptyObject } from '@jest/expect-utils';
+import { isEmptyArray } from '@chakra-ui/utils';
+import { isEmpty } from 'lodash';
 //This is a functional keypad with test data
 export default function Keypad () {
   const pins = ['123', '1234', '88888888', '1111', '1234567890'];
+  const maxPinLength = 8;
   const [pin, setPin] = useState('');
   const[punchMessage, setPunchMessage] = useState();
   const [date, setDate] = useState(new Date());
@@ -30,31 +34,31 @@ export default function Keypad () {
       {punchMessage}
       <Input type='text' readOnly variant="outline" size="lg"  value={pin} onChange={e => setPin(e.target.value)}/>
       <Grid templateColumns="repeat(3, 1fr)" gap={6} column={3} row={4}>
-        <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '1') : setPin(pin)}}>
+        <Button variant="solid" size="md" onClick={() => {(pin.length < maxPinLength) ? setPin(pin + '1') : setPin(pin)}}>
           1
         </Button>
-        <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '2') : setPin(pin)}}>
+        <Button variant="solid" size="md" onClick={() => {(pin.length < maxPinLength) ? setPin(pin + '2') : setPin(pin)}}>
           2
         </Button>
-        <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '3') : setPin(pin)}}>
+        <Button variant="solid" size="md" onClick={() => {(pin.length < maxPinLength) ? setPin(pin + '3') : setPin(pin)}}>
           3
         </Button>
-        <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '4') : setPin(pin)}}>
+        <Button variant="solid" size="md" onClick={() => {(pin.length < maxPinLength) ? setPin(pin + '4') : setPin(pin)}}>
           4
         </Button>
-        <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '5') : setPin(pin)}}>
+        <Button variant="solid" size="md" onClick={() => {(pin.length < maxPinLength) ? setPin(pin + '5') : setPin(pin)}}>
           5
         </Button>
-        <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '6') : setPin(pin)}}>
+        <Button variant="solid" size="md" onClick={() => {(pin.length < maxPinLength) ? setPin(pin + '6') : setPin(pin)}}>
           6
         </Button>
-        <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '7') : setPin(pin)}}>
+        <Button variant="solid" size="md" onClick={() => {(pin.length < maxPinLength) ? setPin(pin + '7') : setPin(pin)}}>
           7
         </Button>
-        <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '8') : setPin(pin)}}>
+        <Button variant="solid" size="md" onClick={() => {(pin.length < maxPinLength) ? setPin(pin + '8') : setPin(pin)}}>
           8
         </Button>
-        <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '9') : setPin(pin)}}>
+        <Button variant="solid" size="md" onClick={() => {(pin.length < maxPinLength) ? setPin(pin + '9') : setPin(pin)}}>
           9
         </Button>
         <Button variant="solid" size="md" colorScheme="linkedin" backgroundColor="gray.700" onClick={() => {setPin('')}}>
@@ -63,23 +67,15 @@ export default function Keypad () {
         <Button variant="solid" size="md" onClick={() => {(pin.length < 8) ? setPin(pin + '0') : setPin(pin)}}>
           0
         </Button>
-        <Button variant="solid" size="md" colorScheme="red" onClick={()=> {
-          if(pin.length > 0) {
-            setPin(pin.substring(0, pin.length-1))
-          }
-         }}>
+        <Button variant="solid" size="md" colorScheme="red" onClick={()=> {if(pin.length > 0) {setPin(pin.substring(0, pin.length-1))}}}>
           {'\u232B'}  
         </Button>
       </Grid>
       <Container display="flex" flexDirection="column">
-          <Button variant="solid" size="lg" colorScheme="linkedin" m={2} onClick={()=> {
+          <Button variant="solid" size="lg" colorScheme="linkedin" m={2} onClick={async ()=> {
           var punch = false;
-          if(pin.length >= 4) {
-            for(var i = 0; i < pins.length; i++){
-              if(pin === pins[i]) {
-                punch = true;
-              }
-            }
+          if(pin.length >= 4 && await checkPin(pin)) {
+            punch = true;
           }
           
           if(punch) setPunchMessage(<Text backgroundColor="whatsapp.100">Clock In Successful!</Text>);
@@ -87,16 +83,11 @@ export default function Keypad () {
         }}>
             Clock In
           </Button>
-          <Button variant="solid" size="lg" colorScheme="linkedin" m={2} onClick={()=> {
+          <Button variant="solid" size="lg" colorScheme="linkedin" m={2} onClick={async ()=> {
           var punch = false;
-          if(pin.length >= 4) {
-            for(var i = 0; i < pins.length; i++){
-              if(pin === pins[i]) {
-                punch = true;
-              }
-            }
+          if(pin.length >= 4 && await checkPin(pin)) {
+            punch = true;
           }
-          
           if(punch) setPunchMessage(<Text backgroundColor="whatsapp.100">Clock Out Successful!</Text>);
           else setPunchMessage(<Text backgroundColor="red.200">Clock Out Failed!</Text>);
         }}>
@@ -106,6 +97,11 @@ export default function Keypad () {
     </InputGroup>
   </Container></ChakraProvider>
    );
-   
+}
+
+async function checkPin(pin) {
+  let query = "SELECT PIN FROM USER WHERE PIN = " + pin;
+  let result = await window.api.selectData(query);
+  return !isEmptyArray(result);
 }
 
