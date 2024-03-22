123bd2c51843d92d3d81e78eabdabb88a264449c
andrewmclaughlan <112527920+andrewmclaughlan@users.noreply.github.com>
2024-03-21 20:56:16 -0600

    timerequests now can insert data into the database. Bug that needs to fix is the fields reset on submit

--- src/time-request.jsx
+++ src/time-request.jsx
@@ -9,6 +9,7 @@ import {
   Button
 } from '@chakra-ui/react'
 
+import { isEmptyArray } from '@chakra-ui/utils';
 export default function TimeRequest() {
   const[submitMessage, setSubmitMessage] = useState();
   var start;
@@ -31,33 +32,47 @@ export default function TimeRequest() {
       <Text>Request Code</Text>
       <Input type='text' onChange={e => (requestCode = e.target.value)}/>
       <Text>Pin</Text>
-      <Input type='password' minLength={4} maxLength={9} onChange={e => (pin = e.target.value)}/>
+      <Input type='password' minLength={4} maxLength={8} onChange={e => (pin = e.target.value)}/>
     </Grid>
     <Container display="flex" flexDirection="row-reverse" justifyContent="flex-start" alignItems="stretch" minWidth="100%" margin={15}>
-      <Button variant="solid" size="md" ml={15} mr={15} onClick={e => (submitButtonHandler(start, end, hours, requestCode, pin))}>Submit</Button>
+      <Button variant="solid" size="md" ml={15} mr={15} onClick={async () => {
+        let result = await submitButtonHandler(start, end, hours, requestCode, pin);
+        if(result === 0) { 
+          setSubmitMessage(<Text backgroundColor="whatsapp.100">Time Request was Successfully Submitted!</Text>); 
+        }
+        if(result === 1) setSubmitMessage(<Text backgroundColor="red.200">Pin was invalid!</Text>);
+        if(result === 2) setSubmitMessage(<Text backgroundColor="red.200">1 or more fields were not filled in!</Text>);
+      }}>Submit</Button>
+    </Container>
+    <Container display="flex" flexDirection="row-reverse" justifyContent="flex-start" alignItems="stretch" minWidth="100%" margin={15}>
+      {submitMessage}
     </Container>
   </ChakraProvider>
   ); 
 }
-function submitButtonHandler(start, end, hours, requestCode, pin) {
-  console.log("Request: \n" + start + "\n" + end + "\n" + hours + "\n" + requestCode + "\n" + pin);
-  if(!(start === undefined & end === undefined && hours === undefined && requestCode === undefined && pin === undefined)) {
-    if(/^\d+$/.test(pin) && pin.length > 4) {
-      if(checkPin()) {
-        console.log('Pin is a number');
-      }
-      
+async function submitButtonHandler(start, end, hours, timeCode, pin) {
+  console.log("Request: \n" + start + "\n" + end + "\n" + hours + "\n" + timeCode + "\n" + pin);
+  if(!(start === undefined || end === undefined || hours === undefined || timeCode === undefined || pin === undefined)) {
+    if(pin.length >= 4 && /^\d+$/.test(pin) && await checkPin(pin)) {
+      var query = "SELECT USERID FROM USER WHERE PIN = " + pin;
+      var result =  await window.api.selectData(query);
+      query = "INSERT INTO TIMERECORDS(START, END, TIMECODE, HOURS, USERID)"
+        +" VALUES('"+ start +"', '" + end + "', '" + timeCode + "', " + hours + ", "+ result[0].userId+")";
+      await window.api.insertData(query);
+       return 0;
     }
     else {
-      console.log(typeof pin);
+      return 1;
     }
   }
-  
+  else {
+    return 2;
+  }
 }
 async function checkPin(pin) {
   let query = "SELECT PIN FROM USER WHERE PIN = " + pin;
   let result = await window.api.selectData(query);
   return !isEmptyArray(result);
 }
-//if(punch) setPunchMessage(<Text backgroundColor="whatsapp.100">Clock Out Successful!</Text>);
-//else setPunchMessage(<Text backgroundColor="red.200">Clock Out Failed!</Text>);
+//if(punch)  setSubmitMessage(<Text backgroundColor="whatsapp.100">Time Request was Successfully Submitted!</Text>);
+//else setSubmitMessage(<Text backgroundColor="red.200">Pin was invalid!</Text>);
